56dc7fcb4a376ee3504c323d0883e961ba3135c2
Santiago <97145522+santiago883@users.noreply.github.com>
2024-03-22 20:34:41 +0000

    adicionado projeto proprio

---
+++ myProject.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main(){
+    int num1, num2, operador;
+    char operador1 = '+';
+    char operador2 = '-';
+    char operador3 = '*';
+    char operador4 = '/';
+    char operador5 = 'x';
+
+    printf("digite um numero: ");
+    scanf("%d", &num1 );
+    printf("digite um operador matematico '+ - * /': ");
+    scanf("%d", &operador);
+    printf("%c", &operador);
+    printf("%c", &operador1);
+    
+    if(operador = operador1 ){
+        printf("entrou ");
+
+
+    }
+
+    
+
+    // //Realizando as operações matematicas
+    // int soma = num1 + num2;
+    // int subtracao = num1 - num2;
+    // int multiplicacao = num1 * num2;
+    // int divisao = num1 / num2;
+    // int resto = num1 % num2; //Resto da divisão
+    
+    // //imprime os resultados
+    // printf("Soma: %d + %d = %d\n", num1, num2, soma);
+    // printf("Subtração: %d - %d = %d\n", num1, num2, subtracao);
+    // printf("Multiplicação %d * %d = %d\n", num1, num2, multiplicacao);
+    // printf("Divisão %d / %d = %d (quociente), Resto: %d\n", num1, num2, divisao, resto);
+
+    // return 0;
+
+}
