fb48cdc2be33ecf3eee213ee7db61c91841b9cd2
Santiago <97145522+santiago883@users.noreply.github.com>
2024-03-26 19:04:29 +0000

    projeto proprio 0.2

--- myProject.cpp
+++ myProject.cpp
@@ -1,34 +1,76 @@
 #include <iostream>
 
-int main(){
-    int num1, num2;
-    char operador;
-    
-
-    printf("digite um numero: ");
-    scanf("%d", num1 );
-    printf("digite um operador matematico '+ - * /': ");
-    scanf("%c", &operador);
-    
-    printf("%d", num2);
-    
-    switch (operador)
-    {
-    case operador == '+':
-       /* code */
+int validar_operador(char x){
+    switch(x){
+    case  '+':
+       return 1;
         break;
-    case operador == '-':
-        /* code */
+    case '-':
+        return 1;
         break;
-    case operador == '*' || operador == 'x':
-        /* code */
+    case '*' || 'x':
+        return 1;
         break;
-    case operador == '/':
-        /* code */
+    case '/':
+        return 1;
         break;
      default:
+        return 2;
         break;
      }
+}
+
+int outroNumero(int num2){
+    printf("digite outro numero \n");
+    scanf("%d", &num2);
+    return num2 = num2;
+}
+
+int main(){
+    int num1, num2, v;
+    char ope;
+
+    // printf("digite um operador matematico '+ - * /':  \n ");
+    // scanf("%c", &ope);
+
+    printf("digite um numero: ");
+    scanf("%d", &num1 );
+
+    printf("digite um operador matematico '+ - * /':  \n ");
+    scanf(" %c", &ope);
+    
+    
+    v = validar_operador(ope);
+    num2 = outroNumero(num2);
+
+    
+    if(v = 1){
+        switch(ope){
+    case  '+':
+       num1 = num1 + num2;
+       printf("resultado: %d \n", num1);
+
+        break;
+    case '-':
+        num1 = num1 - num2;
+        printf("resultado: %d \n", num1);
+        break;
+    case '*' || 'x':
+        num1 = num1 * num2;
+        break;
+    case '/':
+        num1 = num1 / num2;
+        break;
+     default:
+        
+        break;
+     
+    }
+
+    }
+    
+    
+    
     
 
     
