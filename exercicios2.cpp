3ebaab82c6e7cf0d2bd20c50f9c2bab6f97d31ae
Santiago <97145522+santiago883@users.noreply.github.com>
2024-03-26 15:52:26 +0000

    exerxios2 completo

--- exercicios2.cpp
+++ exercicios2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #define RED     "\033[31m"
 #define YELLOW  "\033[33m"
 #define BLUE    "\033[34m"
@@ -11,6 +12,8 @@ int main(){
     // }else{
     //     printf("afirmação 1 é falsa");
     // }
+
+    printf("exercicio 1 \n");
 
     printf("afirmação 1 = %d \n",(5 > 3 && 5 < 10));
 
@@ -29,28 +32,34 @@ int main(){
     printf("afirmação 2 = %d \n",( 8 + 9 != 17));
 
 
-    //exercicio 2
+    printf("exercicio 2 \n");
 
-    // float peso, altura;
+    float peso, altura;
 
-    // printf("digite seu peso em quilogramas: ");
-    // scanf("%f", &peso);
-    // printf("digite a sua altura em metros: ");
-    // scanf("%f", &altura);
-    // printf("%f, %f", peso, altura); 
-    // float resul = (peso / (altura * altura));
+    printf("digite seu peso em quilogramas: ");
+    scanf("%f", &peso);
+    printf("digite a sua altura em metros: ");
+    scanf("%f", &altura);
+    float resul = (peso / (altura * altura));
 
 
-    // printf("Seu indice de masa corporal é: %f",(peso / (altura * altura)));
+    printf("Seu indice de masa corporal é: %f \n",(peso / (altura * altura)));
 
-    //exercicio 3
+    printf("exercicio 3 \n");
     float tem, con;
 
-    printf("digite uma temperatura em celsius");
+    printf("digite uma temperatura em celsius: ");
     scanf("%f", &tem);
-    printf("%f Fahrenheit", (tem = (tem * 9/5)+32));
-    print()
-
+    con = (tem * 9/5)+32;
+    
+    if (tem <= 18) {
+    printf(BLUE "Fahrenheit %f  \n", con);
+  }else if(tem <= 24){
+    printf(YELLOW "Fahrenheit %f  \n", con);
+  }else if(tem > 24){
+    printf(RED "Fahrenheit %f  \n", con);
+  }
+  
 
     return 0;
 }
